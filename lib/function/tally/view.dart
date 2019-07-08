bb90083df3ddff34db17e514ea868e708af3d3bf
张瑞宇 <15201231801@163.com>
2019-07-08 19:03:23 +0800

    记一记tags读取

--- lib/function/tally/view.dart
+++ lib/function/tally/view.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chongmeng/constants/page_constants.dart';
+import 'package:chongmeng/utils/color_utils.dart';
 import 'package:chongmeng/widget/Toolbar.dart';
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
@@ -23,6 +25,53 @@ Widget buildView(TallyState state, Dispatch dispatch, ViewService viewService) {
             ),
           ),
         )
+      ],
+    ),
+    body: CustomScrollView(
+      slivers: <Widget>[
+        SliverList(
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return InkWell(
+              onTap: () {},
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      CachedNetworkImage(
+                        width: 60.0,
+                        height: 60.0,
+                        imageUrl:
+                            "http://mengchong-1253631018.picbj.myqcloud.com/",
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Chip(
+                              label: Text("标签"),
+                              backgroundColor:
+                                  ColorUtils.randomColor(index.toString()),
+                            ),
+                            Text("备注"),
+                          ],
+                        ),
+                      ),
+                      Text("价格"),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("预计时间"),
+                      Text("创建时间"),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }, childCount: 20),
+        ),
       ],
     ),
   );
