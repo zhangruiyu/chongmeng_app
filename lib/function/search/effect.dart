a6e5ad0f3a26b09ed63a7530688db02f6024f580
张瑞宇 <15201231801@163.com>
2019-07-30 15:05:18 +0800

    bugfix

--- lib/function/search/effect.dart
+++ lib/function/search/effect.dart
@@ -2,6 +2,7 @@ import 'package:chongmeng/constants/constants.dart';
 import 'package:chongmeng/network/net_work.dart';
 import 'package:chongmeng/utils/keyboard_utils.dart';
 import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter/material.dart' hide Action;
 import 'action.dart';
 import 'model/search_result_entity.dart';
 import 'state.dart';
@@ -29,7 +30,7 @@ Future _onRefresh(Action action, Context<SearchState> ctx) async {
     'index': 1
   });
   if (result.hasSuccess) {
-    KeyboardUtils.hide();
+    KeyboardUtils.hideByContext(ctx.context);
     ctx.dispatch(SearchActionCreator.onResetData(result.data.data));
   }
 }
@@ -43,7 +44,7 @@ Future _onLoadMore(Action action, Context<SearchState> ctx) async {
   });
   action.payload['completer']();
   if (result.hasSuccess) {
-    KeyboardUtils.hide();
+    KeyboardUtils.hideByContext(ctx.context);
     ctx.dispatch(SearchActionCreator.onAddData(result.data.data, index));
   }
 }
--- lib/utils/keyboard_utils.dart
+++ lib/utils/keyboard_utils.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class KeyboardUtils {
+  //慎用,会导致输入删除问题
   static void hide() {
     SystemChannels.textInput.invokeMethod('TextInput.hide');
+  }
+
+  static void hideByContext(context) {
+    FocusScope.of(context).requestFocus(FocusNode());
   }
 }
