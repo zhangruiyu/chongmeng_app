f2417115c66abc4efd6d216d22828fba049b54e1
张瑞宇 <15201231801@163.com>
2019-08-06 14:58:39 +0800

    优化

--- lib/function/search/view.dart
+++ lib/function/search/view.dart
@@ -5,7 +5,8 @@ import 'package:chongmeng/widget/sliver_app_bar_delegate.dart';
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
-
+import 'package:extended_nested_scroll_view/extended_nested_scroll_view.dart'
+    as extended;
 import 'action.dart';
 import 'state.dart';
 
@@ -14,103 +15,102 @@ Widget buildView(
   var of = Theme.of(viewService.context);
   var buildAdapter = viewService.buildAdapter();
   return Scaffold(
-      body: NestedScrollView(
-    body: EasyRefresh.custom(
-//            enableControlFinishLoad: true,
-//            enableControlFinishRefresh: true,
-      onRefresh: CompleterUtils.produceCompleterAction(
-        dispatch,
-        SearchActionCreator.onRefresh,
-      ),
-      onLoad: CompleterUtils.produceCompleterAction(
-        dispatch,
-        SearchActionCreator.onLoadMore,
-      ),
-      slivers: <Widget>[
-        if (state.pageState == SearchState.EMPTY ||
-            state.pageState == SearchState.INIT)
-          SliverToBoxAdapter(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: buildEmptyView(state, dispatch)),
-          ),
-        if (state.pageState == SearchState.HASDATA)
-          SliverList(
-            delegate: SliverChildBuilderDelegate(buildAdapter.itemBuilder,
-                childCount: buildAdapter.itemCount),
-          )
-      ],
-    ),
-    headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-      return [
-        SliverAppBar(
-          backgroundColor: of.accentColor,
-          pinned: true,
-          elevation: 0.0,
-          floating: true,
-          primary: true,
-          leading: new IconButton(
-              padding: const EdgeInsets.all(0.0),
-              icon: Icon(Icons.arrow_back),
-              tooltip: MaterialLocalizations.of(context).backButtonTooltip,
-              onPressed: () {
-                Navigator.maybePop(context);
-              }),
-          actions: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(right: 22.0),
-              child: InkResponse(
-                onTap: () {
-                  dispatch(SearchActionCreator.onSearch());
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text("搜索"),
+    body: extended.NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return [
+          SliverAppBar(
+            backgroundColor: of.accentColor,
+            pinned: true,
+            elevation: 0.0,
+            floating: true,
+            primary: true,
+            leading: new IconButton(
+                padding: const EdgeInsets.all(0.0),
+                icon: Icon(Icons.arrow_back),
+                tooltip: MaterialLocalizations.of(context).backButtonTooltip,
+                onPressed: () {
+                  Navigator.maybePop(context);
+                }),
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 22.0),
+                child: InkResponse(
+                  onTap: () {
+                    dispatch(SearchActionCreator.onSearch());
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text("搜索"),
+                  ),
+                ),
+              )
+            ],
+            title: Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    new Icon(
+                      Icons.search,
+                      size: 25.0,
+                      color: of.accentColor,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        scrollPadding: const EdgeInsets.all(0.0),
+                        controller: state.textEditingController,
+                        textInputAction: TextInputAction.search,
+                        onSubmitted: (str) {
+                          dispatch(SearchActionCreator.onSearch());
+                        },
+                        decoration: InputDecoration(
+                            contentPadding: EdgeInsets.zero,
+                            border: InputBorder.none,
+                            hintText: "",
+                            hintStyle:
+                                TextStyle(color: colorD1D0D0, fontSize: 12.0)),
+                        autofocus: true,
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            )
-          ],
-          title: Container(
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Row(
-                children: <Widget>[
-                  new Icon(
-                    Icons.search,
-                    size: 25.0,
-                    color: of.accentColor,
-                  ),
-                  Expanded(
-                    child: TextField(
-                      scrollPadding: const EdgeInsets.all(0.0),
-                      controller: state.textEditingController,
-                      textInputAction: TextInputAction.search,
-                      onSubmitted: (str) {
-                        dispatch(SearchActionCreator.onSearch());
-                      },
-                      decoration: InputDecoration(
-                          contentPadding: EdgeInsets.zero,
-                          border: InputBorder.none,
-                          hintText: "",
-                          hintStyle:
-                              TextStyle(color: colorD1D0D0, fontSize: 12.0)),
-                      autofocus: true,
-                      style: TextStyle(fontSize: 14.0),
-                    ),
-                  ),
-                ],
-              ),
+              decoration: BoxDecoration(
+                  color: Color(0x96ffffff),
+                  borderRadius: BorderRadius.all(Radius.circular(30.0))),
             ),
-            decoration: BoxDecoration(
-                color: Color(0x96ffffff),
-                borderRadius: BorderRadius.all(Radius.circular(30.0))),
-          ),
-        )
-      ];
-    },
-  ));
+          )
+        ];
+      },
+      body: EasyRefresh.custom(
+        onRefresh: CompleterUtils.produceCompleterAction(
+          dispatch,
+          SearchActionCreator.onRefresh,
+        ),
+        onLoad: CompleterUtils.produceCompleterAction(
+          dispatch,
+          SearchActionCreator.onLoadMore,
+        ),
+        slivers: <Widget>[
+          if (state.pageState == SearchState.EMPTY ||
+              state.pageState == SearchState.INIT)
+            SliverToBoxAdapter(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: buildEmptyView(state, dispatch)),
+            ),
+          if (state.pageState == SearchState.HASDATA)
+            SliverList(
+              delegate: SliverChildBuilderDelegate(buildAdapter.itemBuilder,
+                  childCount: buildAdapter.itemCount),
+            )
+        ],
+      ),
+    ),
+  );
 }
 
 List<Row> buildEmptyView(SearchState state, Dispatch dispatch) {
