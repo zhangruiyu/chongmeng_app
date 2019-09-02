44debdb3c6f372fa0e38ac700083e72b52824dd7
张瑞宇 <15201231801@163.com>
2019-09-02 14:34:17 +0800

    ios证书

--- lib/function/safe_center/view.dart
+++ lib/function/safe_center/view.dart
@@ -1,9 +1,15 @@
+import 'package:chongmeng/widget/Toolbar.dart';
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
 
 import 'action.dart';
 import 'state.dart';
 
-Widget buildView(SafeCenterState state, Dispatch dispatch, ViewService viewService) {
-  return Container();
+Widget buildView(
+    SafeCenterState state, Dispatch dispatch, ViewService viewService) {
+  return Scaffold(
+    appBar: Toolbar(
+      title: Text("安全中心"),
+    ),
+  );
 }
