f220bca9821ac5ef7738582b2449ce86df4882d4
张瑞宇 <15201231801@163.com>
2019-10-20 17:09:13 +0800

    展示

--- lib/function/my_order/model/my_order_entity.dart
+++ lib/function/my_order/model/my_order_entity.dart
@@ -33,9 +33,12 @@ class MyOrderData {
   String consigneeName;
   String cityname;
   int count;
+  String sourceType;
   List<String> pic;
   String remake;
+  String type;
   String addressDetail;
+  List<MyOrderDataVirtualProduct> virtualProduct;
   String provincename;
   int integral;
   String name;
@@ -49,9 +52,12 @@ class MyOrderData {
       this.consigneeName,
       this.cityname,
       this.count,
+      this.sourceType,
       this.pic,
       this.remake,
+      this.type,
       this.addressDetail,
+      this.virtualProduct,
       this.provincename,
       this.integral,
       this.name,
@@ -65,9 +71,17 @@ class MyOrderData {
     consigneeName = json['consignee_name'];
     cityname = json['cityname'];
     count = json['count'];
+    sourceType = json['source_type'];
     pic = json['pic']?.cast<String>();
     remake = json['remake'];
+    type = json['type'];
     addressDetail = json['address_detail'];
+    if (json['virtual_product'] != null) {
+      virtualProduct = new List<MyOrderDataVirtualProduct>();
+      (json['virtual_product'] as List).forEach((v) {
+        virtualProduct.add(new MyOrderDataVirtualProduct.fromJson(v));
+      });
+    }
     provincename = json['provincename'];
     integral = json['integral'];
     name = json['name'];
@@ -83,15 +97,51 @@ class MyOrderData {
     data['consignee_name'] = this.consigneeName;
     data['cityname'] = this.cityname;
     data['count'] = this.count;
+    data['source_type'] = this.sourceType;
     data['pic'] = this.pic;
     data['remake'] = this.remake;
+    data['type'] = this.type;
     data['address_detail'] = this.addressDetail;
+    if (this.virtualProduct != null) {
+      data['virtual_product'] =
+          this.virtualProduct.map((v) => v.toJson()).toList();
+    }
     data['provincename'] = this.provincename;
     data['integral'] = this.integral;
     data['name'] = this.name;
     data['tel'] = this.tel;
     data['id'] = this.id;
     data['status'] = this.status;
+    return data;
+  }
+}
+
+class MyOrderDataVirtualProduct {
+  String invalidTime;
+  String virtualProductRepertoryStatus;
+  String content;
+  String convertTime;
+
+  MyOrderDataVirtualProduct(
+      {this.invalidTime,
+      this.virtualProductRepertoryStatus,
+      this.content,
+      this.convertTime});
+
+  MyOrderDataVirtualProduct.fromJson(Map<String, dynamic> json) {
+    invalidTime = json['invalid_time'];
+    virtualProductRepertoryStatus = json['virtual_product_repertory_status'];
+    content = json['content'];
+    convertTime = json['convert_time'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['invalid_time'] = this.invalidTime;
+    data['virtual_product_repertory_status'] =
+        this.virtualProductRepertoryStatus;
+    data['content'] = this.content;
+    data['convert_time'] = this.convertTime;
     return data;
   }
 }
